df03bcac2a36a13120b1b1f6e929b67d70c7aacd
joaoGUFPR <joaoguilherme1@ufpr,br>
2025-03-25 14:16:38 -0300

    Criação component page/autocadastro

---
+++ trab-web2/src/app/pages/autocadastro/autocadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocadastro',
+  imports: [],
+  templateUrl: './autocadastro.component.html',
+  styleUrl: './autocadastro.component.css'
+})
+export class AutocadastroComponent {
+
+}
