ee814282d1465417c008b142190662b9cc1b2885
joaoGUFPR <joaoguilherme1@ufpr,br>
2025-03-25 14:52:46 -0300

    Criação rota autocadastro

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AutocadastroComponent } from './pages/autocadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'autocadastro', component: AutocadastroComponent}
+];
