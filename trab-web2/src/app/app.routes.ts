d7403eaceebab8b6c39396ab9077ee998436caea
joaoGUFPR <joaoguilherme1@ufpr,br>
2025-03-26 00:17:53 -0300

    Criação rota Pagina Inicial Cliente

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { AutocadastroComponent } from './pages/autocadastro';
+import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
 
 export const routes: Routes = [
-    {path: 'autocadastro', component: AutocadastroComponent}
+    {path: 'autocadastro', component: AutocadastroComponent},
+    {path: 'paginainicialcliente', component: PaginaInicialClienteComponent}
+
 ];
