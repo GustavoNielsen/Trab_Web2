18fab06d44121b67ea4a0185834d7c0ef3fb537c
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2025-03-27 19:24:25 -0300

    Adiciona Solicita Manutenção

---
+++ trab-web2/src/app/pages/solicita-manutencao/solicita-manutencao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicita-manutencao',
+  imports: [],
+  templateUrl: './solicita-manutencao.component.html',
+  styleUrl: './solicita-manutencao.component.css'
+})
+export class SolicitaManutencaoComponent {
+
+}
