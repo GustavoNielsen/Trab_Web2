b27b5d9c145c8d859e5f2ebdd0bb89785c2d66b9
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2025-03-27 19:20:16 -0300

    Correção do arquivo app.components.ts]

--- trab-web2/src/app/app.component.ts
+++ trab-web2/src/app/app.component.ts
@@ -4,13 +4,10 @@ import { AutocadastroComponent } from "./pages/autocadastro/autocadastro.compone
 import { MostrarOrcamentoComponent } from "./pages/mostrar-orcamento/mostrar-orcamento.component";
 import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente/pagina-inicial-cliente.component';
 import { LoginComponent } from './pages/login/login.component';
+
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  imports: [RouterOutlet,AutocadastroComponent, MostrarOrcamentoComponent, PaginaInicialClienteComponent],
-=======
-  imports: [RouterOutlet, AutocadastroComponent, PaginaInicialClienteComponent, LoginComponente, MostrarOrcamentoComponent],
->>>>>>> 9bd4b8b6a3e05922684fe320ed7509be979e3884
+  imports: [RouterOutlet, AutocadastroComponent, PaginaInicialClienteComponent, LoginComponent, MostrarOrcamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
