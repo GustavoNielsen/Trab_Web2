1c84f4717ff7af312d3a9d6c0c5f0654fc363f2f
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2025-03-26 21:30:28 -0300

    Correção nas rotas de componentes

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -1,18 +1,10 @@
 import { Routes } from '@angular/router';
 import { AutocadastroComponent } from './pages/autocadastro';
-<<<<<<< HEAD
 import { MostrarOrcamentoComponent } from './pages/mostrar-orcamento';
+import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
 
 export const routes: Routes = [//
     {path: 'autocadastro', component: AutocadastroComponent},
-    {path: 'mostrar-orcamento', component: MostrarOrcamentoComponent}
-];
-=======
-import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
-
-export const routes: Routes = [
-    {path: 'autocadastro', component: AutocadastroComponent},
+    {path: 'mostrar-orcamento', component: MostrarOrcamentoComponent},
     {path: 'paginainicialcliente', component: PaginaInicialClienteComponent}
-
 ];
->>>>>>> 23551bea3a18e49cf52ef22ee3e9e52c2beb60e6
