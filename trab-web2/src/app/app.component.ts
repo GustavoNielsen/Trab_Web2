e56934f9a185c87cba14367c317da52b5b2cacfa
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2025-04-02 21:54:11 -0300

    Criação efetuar-orcamento.component, alterações app.component.ts, app.routes.ts, mostrar-orcamento.component

--- trab-web2/src/app/app.component.ts
+++ trab-web2/src/app/app.component.ts
@@ -4,10 +4,28 @@ import { AutocadastroComponent } from "./pages/autocadastro/autocadastro.compone
 import { MostrarOrcamentoComponent } from "./pages/mostrar-orcamento/mostrar-orcamento.component";
 import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente/pagina-inicial-cliente.component';
 import { LoginComponent } from './pages/login/login.component';
+<<<<<<< Updated upstream
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, AutocadastroComponent, PaginaInicialClienteComponent, LoginComponent, MostrarOrcamentoComponent],
+=======
+import { VizualizarServicoComponent } from './pages/vizualizar-servico/vizualizar-servico.component';
+import { SolicitaManutencaoComponent } from './pages/solicita-manutencao';
+import { EfetuarOrcamentoComponent } from './pages/efetuar-orcamento/efetuar-orcamento.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, 
+            AutocadastroComponent, 
+            PaginaInicialClienteComponent, 
+            LoginComponent, 
+            MostrarOrcamentoComponent,
+            VizualizarServicoComponent, 
+            SolicitaManutencaoComponent,
+            EfetuarOrcamentoComponent],
+            
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
