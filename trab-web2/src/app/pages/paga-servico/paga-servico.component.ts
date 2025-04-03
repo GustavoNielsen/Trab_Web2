fd4d17c470ebf9e25e4266721dac6423ea5e9f2d
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2025-04-03 20:35:54 -0300

    feat(R10): implementa tela pagar serviço
    Criação dos componentes para implementação da tela de pagamento do serviço.

---
+++ trab-web2/src/app/pages/paga-servico/paga-servico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paga-servico',
+  imports: [],
+  templateUrl: './paga-servico.component.html',
+  styleUrl: './paga-servico.component.css'
+})
+export class PagaServicoComponent {
+
+}
