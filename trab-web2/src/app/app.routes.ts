102bb820277560fc43060bf1843f863978093068
fulcrunn <fulcrunn@yahoo.com.br>
2025-04-04 00:53:54 -0300

    Criação do crud-categoria-equipamento

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { MostrarOrcamentoComponent } from './pages/mostrar-orcamento';
 import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
 import { SolicitaManutencaoComponent } from './pages/solicita-manutencao';
 import { LoginComponent } from './pages/login/login.component';
+import {CrudCategoriaEquipamentoComponent} from './pages/crud-categoria-equipamento/crud-categoria-equipamento.component';
 
 export const routes: Routes = [
     {path: 'autocadastro', component: AutocadastroComponent},
     {path: 'mostrar-orcamento', component: MostrarOrcamentoComponent},
     {path: 'paginainicialcliente', component: PaginaInicialClienteComponent},
     {path: 'solicitamanutecao', component: SolicitaManutencaoComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'crudcategoriaequipamento', component: CrudCategoriaEquipamentoComponent}
 ];
