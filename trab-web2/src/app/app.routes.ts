f37b44ee462ee758d446c48b7e359ba8a006c92d
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2025-04-06 15:28:43 -0300

    Correção pós merge

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -4,18 +4,19 @@ import { MostrarOrcamentoComponent } from './pages/mostrar-orcamento';
 import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
 import { SolicitaManutencaoComponent } from './pages/solicita-manutencao';
 import { LoginComponent } from './pages/login/login.component';
-import {CrudCategoriaEquipamentoComponent} from './pages/crud-categoria-equipamento/crud-categoria-equipamento.component';
+import { CrudCategoriaEquipamentoComponent } from './pages/crud-categoria-equipamento/crud-categoria-equipamento.component';
 import { VisualizarSolicitacaoComponent } from './pages/visualizar-solicitacao/visualizar-solicitacao.component';
+import { EfetuarOrcamentoComponent } from './pages/efetuar-orcamento/efetuar-orcamento.component'
 
 export const routes: Routes = [
     {path: 'autocadastro', component: AutocadastroComponent},
-    {path: 'mostrar-orcamento', component: MostrarOrcamentoComponent},
+    {path: 'mostrarorcamento', component: MostrarOrcamentoComponent},
     {path: 'paginainicialcliente', component: PaginaInicialClienteComponent},
     {path: 'solicitamanutecao', component: SolicitaManutencaoComponent},
     {path: 'login', component: LoginComponent},
     {path: 'crudcategoriaequipamento', component: CrudCategoriaEquipamentoComponent},
     {path: 'visualizarsolicitacao', component: VisualizarSolicitacaoComponent},
-    {path: 'paginainicialcliente', component: PaginaInicialClienteComponent}
+    {path: 'efetuarorcamento', component: EfetuarOrcamentoComponent}
 
 ];
     //{path: 'login', component: LoginComponent}
