e7d0bd77791efe37eed114902c7c21da0128e765
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2025-04-10 20:50:09 -0300

    feat(RF09 e RF010): Adiciona Rotas tela de pagamento e serviço

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { AutocadastroComponent } from './pages/autocadastro';
 import { MostrarOrcamentoComponent } from './pages/mostrar-orcamento';
 import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
+import { PagaServicoComponent } from './pages/paga-servico';
+import { ResgataServicoComponent } from './pages/resgata-servico';
 
 export const routes: Routes = [
     {path: 'autocadastro', component: AutocadastroComponent},
     {path: 'mostrar-orcamento', component: MostrarOrcamentoComponent},
-    {path: 'paginainicialcliente', component: PaginaInicialClienteComponent}
+    {path: 'paginainicialcliente', component: PaginaInicialClienteComponent},
+    {path: 'paga-servico', component: PagaServicoComponent},
+    {path: 'resgata-servico', component: ResgataServicoComponent}
+
     //{path: 'login', component: LoginComponent}
 ];
---
+++ trab-web2/src/app/pages/resgata-servico/index.ts
@@ -0,0 +1,1 @@
+export * from './resgata-servico.component'
