7fb2c1fb346dd5c3e88afc25e689d6a3873b0889
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2025-04-10 21:35:37 -0300

    feat(RF19): Cria rotas relatorio de receita

--- trab-web2/src/app/app.routes.ts
+++ trab-web2/src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { MostrarOrcamentoComponent } from './pages/mostrar-orcamento';
 import { PaginaInicialClienteComponent } from './pages/pagina-inicial-cliente';
 import { PagaServicoComponent } from './pages/paga-servico';
 import { ResgataServicoComponent } from './pages/resgata-servico';
+import { RelatorioReceitasComponent } from './pages/relatorio-receitas';
 
 export const routes: Routes = [
     {path: 'autocadastro', component: AutocadastroComponent},
     {path: 'mostrar-orcamento', component: MostrarOrcamentoComponent},
     {path: 'paginainicialcliente', component: PaginaInicialClienteComponent},
     {path: 'paga-servico', component: PagaServicoComponent},
-    {path: 'resgata-servico', component: ResgataServicoComponent}
+    {path: 'resgata-servico', component: ResgataServicoComponent},
+    {path: 'relatorio-receita', component:RelatorioReceitasComponent}
 
     //{path: 'login', component: LoginComponent}
 ];
