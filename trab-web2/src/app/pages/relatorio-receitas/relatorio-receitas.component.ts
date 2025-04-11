27898d14b6c09d0b2443c8c080027efc3e33ccc8
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2025-04-10 21:31:23 -0300

    feat(RF19): Cria componentes relatorio de receitas

---
+++ trab-web2/src/app/pages/relatorio-receitas/relatorio-receitas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio-receitas',
+  imports: [],
+  templateUrl: './relatorio-receitas.component.html',
+  styleUrl: './relatorio-receitas.component.css'
+})
+export class RelatorioReceitasComponent {
+
+}
