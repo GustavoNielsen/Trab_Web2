96a2604edf95b1f8d243762657cbfc8ba2326357
ruanito42 <ruansergio.cunha@gmail.com>
2025-04-11 00:22:36 -0300

    RF05 e RF06 - Pagina de orçamento - modais de aprova e rejeita serviço

--- trab-web2/src/app/pages/visualizar-servico/visualizar-servico.component.ts
+++ trab-web2/src/app/pages/visualizar-servico/visualizar-servico.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-visualizar-servico',
@@ -8,22 +8,10 @@ import Swal from 'sweetalert2';
   styleUrl: './visualizar-servico.component.css'
 })
 export class VisualizarServicoComponent {
-  valorServico: string = '1000000';
+ 
 
   
 
-  aprovarServico() {
-    Swal.fire({
-      title: 'Serviço Aprovado!',
-      text: 'Serviço Aprovado no Valor R$ ' + this.valorServico,
-      icon: 'success',
-      confirmButtonText: 'OK'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        console.log('funfou');
-    //    this.router.navigate(['']);//colocar rota
-      }
-    });
-  }
+  
 }
 
