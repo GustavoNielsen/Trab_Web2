4ebdb9fc43bc828b7680c3583a28b6279db7f0dd
GustavoNielsen <142043434+GustavoNielsen@users.noreply.github.com>
2025-04-23 22:51:36 -0300

    Feat(RF004)Implementa Método enviarSolcitacao()

--- trab-web2/src/app/pages/solicita-manutencao/solicita-manutencao.component.ts
+++ trab-web2/src/app/pages/solicita-manutencao/solicita-manutencao.component.ts
@@ -33,3 +33,48 @@ export class SolicitaManutencaoComponent implements OnInit {
     private clienteService: ClienteService,
     private equipamentoService: EquipamentoService
   ) {}
+
+
+  ngOnInit(): void {
+    this.cpf = this.clienteService.cpfLogado;
+    this.equipamentos = this.equipamentoService.listarTodos()
+
+  }
+  enviarSolicitacao(): void {
+
+    const agora = new Date();
+    const dataHoraString = agora.toISOString(); 
+
+    const historicoInicial = new Historicosolicitacao(
+      dataHoraString,    
+      'Aberta',          
+      this.id,           
+      'Solicitação criada' 
+    );
+
+    const novaSolicitacao = new Solicitacao(
+      dataHoraString,
+      this.descricaoEquipamento,
+      this.categoriaEquipamento,
+      this.descricaoDefeito,
+      'Aberta',
+      this.cpf,
+      this.id,
+      0,
+      '',
+      '',
+      '',
+      '',
+      '',
+      [historicoInicial],
+      '',
+      '',
+      '',
+      '',
+      ''
+    );
+    this.solicitacaoService.adicionarSolicitacao(novaSolicitacao);
+    console.log('Solicitação adicionada: ', novaSolicitacao);
+    this.router.navigate(['/paginainicialcliente']);
+  }
+}
