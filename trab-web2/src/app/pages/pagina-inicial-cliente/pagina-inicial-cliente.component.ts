af48ab315cb920486c5e58690ea98cff8c57dbce
Andre Oliveira <fulcrunn@yahoo.com.br>
2025-04-24 23:17:08 -0300

    RF(003) e RF(017) Refactor

--- trab-web2/src/app/pages/pagina-inicial-cliente/pagina-inicial-cliente.component.ts
+++ trab-web2/src/app/pages/pagina-inicial-cliente/pagina-inicial-cliente.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+// pages/pagina-inicial-cliente/pagina-inicial-cliente.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
+import { Solicitacao } from '../../shared/models/solicitacao';
+import { SolicitacaoService } from '../../services/soliciticao.service';
+import { ClienteService } from '../../services/cliente.service'; 
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-pagina-inicial-cliente',
-  imports: [],
   templateUrl: './pagina-inicial-cliente.component.html',
-  styleUrl: './pagina-inicial-cliente.component.css'
+  styleUrls: ['./pagina-inicial-cliente.component.css'], 
+  imports:[CommonModule, NavbarComponent, RouterLink, RouterModule]
 })
-export class PaginaInicialClienteComponent {
+export class PaginaInicialClienteComponent implements OnInit {
+  cpf: string = '';
+  solicitacoes: Solicitacao[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private solicitacaoService: SolicitacaoService,
+    private clienteService: ClienteService 
+  ) {}
+
+  ngOnInit(): void {
+    this.cpf = this.clienteService.cpfLogado
+    console.log(this.cpf)
+    this.solicitacoes = this.solicitacaoService.listarSolicitacoesPorCpf(this.cpf);
+    console.log(this.cpf)
+    this.solicitacoes.sort((a, b) => new Date(a.dataHora).getTime() - new Date(b.dataHora).getTime());
+  }
+
+  visualizarSolicitacao(solicitacao: Solicitacao): void {
+    console.log('Visualizar solicitação', solicitacao);
+  }
+
+  acaoSolicitacao(solicitacao: Solicitacao): void {
+    console.log('Ação na solicitação', solicitacao);
+  }
+  resgatarServico(solicitacao: Solicitacao) {
+    this.solicitacaoService.resgatarSolicitacao(solicitacao.dataHora, solicitacao.valorOrcamento,solicitacao.observacoesOrcamento, solicitacao.idFuncionario)
+    solicitacao.estado = "Aprovada"
+  }
 }
-
