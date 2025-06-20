b375d874b283c548f014bf9ac0289edaa95b9a08
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2025-06-20 18:13:57 -0300

    fix() - Model de Cliente reparada

--- backendtrabweb2/src/main/java/com/example/backend/backendtrabweb2/model/Cliente.java
+++ backendtrabweb2/src/main/java/com/example/backend/backendtrabweb2/model/Cliente.java
@@ -5,123 +5,166 @@ import java.util.Objects;
 
 import jakarta.persistence.*;
 
-
 @Entity
-@Table(name="cliente")
-public class Cliente extends Usuario implements Serializable
-{
-	private static final long serialVersionUID = 1L;
+@Table(name = "tb_cliente")
+public class Cliente {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long idCliente;
-	
-	private String endereco;
-	private String senha;
-	private String email;
-	private String nome;
-	private String cpf;
-	private String telefone;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Cliente() {
-		super();
-	}
+    @Column(name = "email_cliente", nullable = false, unique = true)
+    private String email;
 
-	public Cliente(String endereco, String senha, String email, String nome, String cpf, String telefone) {
-		super();
-		this.endereco = endereco;
-		this.senha = senha;
-		this.email = email;
-		this.nome = nome;
-		this.cpf = cpf;
-		this.telefone = telefone;
-	}
-	
-	public Cliente(Long id, String endereco, String senha, String email, String nome, String cpf, String telefone) {
-		super();
-		this.idCliente = id;
-		this.endereco = endereco;
-		this.senha = senha;
-		this.email = email;
-		this.nome = nome;
-		this.cpf = cpf;
-		this.telefone = telefone;
-	}
+    @Column(name = "senha_cliente", nullable = false)
+    private String senha;
 
+    @Column(name = "nome_cliente", nullable = false)
+    private String nome;
 
-	public Long getIdCliente() {
-		return idCliente;
-	}
+    @Column(name = "tipo", nullable = false)
+    private String tipo;         // ex: 'cliente'
 
-	public void setIdCliente(Long idCliente) {
-		this.idCliente = idCliente;
-	}
+    @Column(name = "cpf_cliente", nullable = false, unique = true, length = 11)
+    private String cpf;
 
-	public String getEndereco() {
-		return endereco;
-	}
+    private String cep;
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String telefone;
 
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
+    public Cliente() {}
 
-	public String getSenha() {
-		return senha;
-	}
+    public Cliente(String email,
+                   String senha,
+                   String nome,
+                   String tipo,
+                   String cpf,
+                   String cep,
+                   String rua,
+                   String numero,
+                   String complemento,
+                   String bairro,
+                   String cidade,
+                   String estado,
+                   String telefone) {
+        this.email       = email;
+        this.senha       = senha;
+        this.nome        = nome;
+        this.tipo        = tipo;
+        this.cpf         = cpf;
+        this.cep         = cep;
+        this.rua         = rua;
+        this.numero      = numero;
+        this.complemento = complemento;
+        this.bairro      = bairro;
+        this.cidade      = cidade;
+        this.estado      = estado;
+        this.telefone    = telefone;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    // getters e setters
 
-	public String getEmail() {
-		return email;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
-	public String getTelefone() {
-		return telefone;
-	}
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(idCliente);
-	}
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		return Objects.equals(idCliente, other.idCliente);
-	}
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 
-	
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
-
